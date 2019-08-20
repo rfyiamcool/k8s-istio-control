6e8c40d4a9266eec49f8fb46736f80804aa38652
rfyiamcool <rfyiamcool@163.com>
2019-08-20 20:58:51 +0800

    for me

--- gen.go
+++ gen.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -25,9 +26,12 @@ var (
 )
 
 const (
-	FLAG_ENABLE  = "enable"
-	FLAG_DISABLE = "disable"
+	FLAG_ENABLE  = "enabled"
+	FLAG_DISABLE = "disabled"
 	FLAG_STATUS  = "status"
+
+	OP_SERV_RUN = iota
+	OP_SERV_GENERATE
 )
 
 type config struct {
@@ -39,6 +43,10 @@ type config struct {
 	Disable           ControlTypes        `yaml:"disable"`
 	MustDeps          []string            `yaml:"must_deps"`
 	SkipInjectService []string            `yaml:"skip_inject_service"`
+
+	HighPriorityDeps []string `yaml:"high_priority_deps"`
+	MidPriorityDeps  []string `yaml:"mid_priority_deps"`
+	LowPriorityDeps  []string `yaml:"low_priority_deps"`
 }
 
 type ControlTypes struct {
@@ -61,7 +69,7 @@ type generator struct {
 	NameSpace      string
 	service        map[string]serviceTypes
 	runningService map[string]bool
-	enableService  map[string]bool
+	enableService  []string
 }
 
 func newGenerator(cfg config) *generator {
@@ -69,15 +77,23 @@ func newGenerator(cfg config) *generator {
 		cfg:            cfg,
 		service:        make(map[string]serviceTypes),
 		runningService: make(map[string]bool),
-		enableService:  make(map[string]bool),
+		enableService:  make([]string, 0),
 	}
 }
 
 func (g *generator) init() {
-	g.preClean()
 	pwd, _ := os.Getwd()
 	g.service = g.walkMeshDir(pwd)
-	g.enableService = g.getEnableServices()
+
+	g.enableService = g.sortPriorityService(
+		g.fillMustDeps(
+			g.getEnableService(),
+		),
+	)
+
+	if sn, repeated := isArrayRepeat(g.enableService); repeated {
+		log.fault("found service %s repeated", sn)
+	}
 
 	ns, _ := g.cfg.Vars["namespace"]
 	if ns == "" {
@@ -90,13 +106,70 @@ func (g *generator) init() {
 	g.NameSpace = ns
 }
 
-func (g *generator) run() {
-	for sn, _ := range g.enableService {
-		log.info("will deploy service %s", sn)
+func (g *generator) selectAndCreateNS() {
+	var (
+		format, cmd string
+		err         error
+	)
 
+	// query namespace
+	format = "kubectl  get ns|awk '{ print $1 }'|grep -v grep|grep %s"
+	cmd = fmt.Sprintf(format, g.NameSpace)
+	stdout, _, err := g.commandExecute(cmd, "/")
+	stdout = removeLine(stdout)
+	if stdout == g.NameSpace && err == nil {
+		return
+	}
+
+	// create
+	format = "kubectl create namespace %s"
+	cmd = fmt.Sprintf(format, g.NameSpace)
+	stdout, stderr, err := g.commandExecute(cmd, "/")
+	if err != nil {
+		log.fault("create namespace failed, stdout: %s, stderr: %s, err: %s",
+			stdout,
+			stderr,
+			err.Error(),
+		)
+	}
+
+	log.info(stdout)
+}
+
+func (g *generator) run() {
+	g.beforeRun()
+	g.selectAndCreateNS()
+	g.handleRun(OP_SERV_RUN)
+	g.afterRun()
+}
+
+func (g *generator) generator() {
+	g.beforeRun()
+	g.handleRun(OP_SERV_GENERATE)
+}
+
+func (g *generator) beforeRun() {
+	g.preClean()
+	g.createShareStorage()
+	g.syncDeps()
+}
+
+func (g *generator) afterRun() {
+}
+
+func (g *generator) createShareStorage() {
+	err := os.Mkdir("/biss-dep", os.ModePerm) // share dep
+	if !existFile("/biss-dep") {
+		log.fault("create share storage failed, err: %s", err.Error())
+	}
+}
+
+func (g *generator) handleRun(op int) {
+	for _, sn := range g.enableService {
 		if state, _ := g.runningService[sn]; state {
 			continue
 		}
+		g.runningService[sn] = true
 
 		serv, ok := g.service[sn]
 		if !ok {
@@ -104,64 +177,118 @@ func (g *generator) run() {
 			errExit()
 		}
 
-		g.prepare(serv)
-		g.generateCfg(serv)
-		g.start(serv.sname)
+		g.servGenerateCfg(serv)
 
-		log.info("finish deploy service %s", sn)
+		if op == OP_SERV_GENERATE {
+			continue
+		}
+
+		g.servStart(serv.sname)
+		log.color(green, "finish deploy service %s", sn)
 	}
 }
 
-func (g *generator) getEnableServices() map[string]bool {
+func (g *generator) fillMustDeps(sl []string) []string {
 	var (
-		services = map[string]bool{}
+		wl = []string{}
 	)
 
-	if len(g.cfg.Enable.Service) == 0 && len(g.cfg.Enable.ServiceGroup) == 0 {
-		for _, sn := range g.cfg.Service {
-			services[sn] = true
+	for _, sn := range g.cfg.MustDeps {
+		if isExistInList(sn, sl) {
+			continue
 		}
-		return services
+		wl = append(wl, sn)
+	}
+
+	// insert front
+	return append(wl, sl...)
+}
+
+func (g *generator) sortPriorityService(sl []string) []string {
+	var (
+		base = []string{}
+		high = []string{}
+		mid  = []string{}
+		low  = []string{}
+	)
+
+	fn := func(sn string, target *[]string, dl []string) bool {
+		if isExistInList(sn, dl) {
+			*target = append(*target, sn)
+			return true
+		}
+		return false
+	}
+
+	for _, sn := range sl {
+		if fn(sn, &high, g.cfg.HighPriorityDeps) {
+			continue
+		}
+		if fn(sn, &mid, g.cfg.MidPriorityDeps) {
+			continue
+		}
+		if fn(sn, &low, g.cfg.LowPriorityDeps) {
+			continue
+		}
+
+		base = append(base, sn)
+	}
+
+	high = append(high, mid...)
+	high = append(high, low...)
+	high = append(high, base...)
+	return high
+}
+
+// base sort and repeat service
+func (g *generator) getEnableService() []string {
+	var (
+		serviceSet = map[string]bool{}
+	)
+
+	// default enable all services
+	if len(g.cfg.Enable.Service) == 0 && len(g.cfg.Enable.ServiceGroup) == 0 {
+		return g.cfg.Service
 	}
 
 	for _, sgn := range g.cfg.Enable.ServiceGroup {
 		sl := g.cfg.ServiceGroup[sgn]
 		for _, sn := range sl {
-			services[sn] = true
+			serviceSet[sn] = true
 		}
 	}
-
 	for _, sn := range g.cfg.Enable.Service {
-		services[sn] = true
+		serviceSet[sn] = true
 	}
 
-	return services
+	sl := []string{}
+	for sn, _ := range serviceSet {
+		sl = append(sl, sn)
+	}
+
+	return sl
 }
 
-func (g *generator) prepare(serv serviceTypes) {
-	if serv.sync == "" {
-		return
-	}
-
+func (g *generator) syncDeps() {
 	var (
 		pwd, _ = os.Getwd()
-		path   = pwd + "/" + serv.sname
 		cmd    = "bash sync.sh"
 	)
 
-	stdout, stderr, err := g.commandExecute(cmd, path)
+	stdout, stderr, err := g.commandExecute(cmd, pwd)
 	if err != nil {
-		log.error("service %s sync.sh failed in prepare, stdout: %s, stderr: %s, err: %s", serv.sname,
+		log.error("bash sync.sh failed in prepare, stdout: %s, stderr: %s, err: %s",
 			stdout,
 			stderr,
 			err.Error(),
 		)
 		errExit()
 	}
+
 	// g.cmdPrint(serv.sname, cmd, stdout, stderr)
 }
 
-func (g *generator) generateCfg(serv serviceTypes) {
+func (g *generator) servGenerateCfg(serv serviceTypes) {
 	tmpls := []string{serv.configMap, serv.dm, serv.service, serv.istio}
 	for _, tp := range tmpls {
 		if tp == "" {
@@ -230,10 +357,6 @@ func (g *generator) validateSyntax(tmpl string) (string, bool) {
 	return "", true
 }
 
-func (g *generator) ExistShareStorage() bool {
-	return true
-}
-
 func (g *generator) commandExecute(cmd string, workPath string) (string, string, error) {
 	var (
 		stdout, stderr bytes.Buffer
@@ -279,7 +402,10 @@ func (g *generator) controlIstioInject(mark string, workPath string) {
 		log.fault("invalid type")
 	}
 
-	cmd := "./scripts/inject_control.sh " + mark
+	cmd := fmt.Sprintf("kubectl label namespace %s istio-injection=%s --overwrite",
+		g.NameSpace,
+		mark,
+	)
 	stdout, stderr, err := g.commandExecute(
 		cmd, workPath,
 	)
@@ -296,12 +422,12 @@ func (g *generator) controlIstioInject(mark string, workPath string) {
 }
 
 func (g *generator) iterateStart() {
-	for sn, _ := range g.enableService {
-		g.start(sn)
+	for _, sn := range g.enableService {
+		g.servStart(sn)
 	}
 }
 
-func (g *generator) start(sn string) {
+func (g *generator) servStart(sn string) {
 	var (
 		pwd, _         = os.Getwd()
 		workPath       = pwd + "/" + g.cfg.OutputPath + "/" + sn
@@ -356,15 +482,21 @@ func (g *generator) restart() {
 }
 
 func (g *generator) stopAll() {
-	format := "kubectl -n %s delete po,svc,configmap,vs,dr --all"
+	format := "kubectl -n %s delete daemonsets,deployment,po,svc,configmap,vs,dr --all --force --grace-period=0"
 	cmd := fmt.Sprintf(format, g.NameSpace)
-	g.commandExecute(cmd, "/")
+	stdout, _, _ := g.commandExecute(cmd, "/")
+	log.info(stdout)
+
+	// cmd = fmt.Sprintf("kubectl delete namespace %s", g.NameSpace)
+	// stdout, _, _ = g.commandExecute(cmd, "/")
+	// log.info(stdout)
 }
 
 func (g *generator) statusAll() {
-	format := "kubectl get po,svc,configmap,vs,dr -n %s"
+	format := "kubectl -n %s get pods,svc,configmap,vs,dr,daemonsets"
 	cmd := fmt.Sprintf(format, g.NameSpace)
-	g.commandExecute(cmd, "/")
+	stdout, _, _ := g.commandExecute(cmd, "/")
+	log.info(stdout)
 }
 
 func (g *generator) walkMeshDir(path string) map[string]serviceTypes {
@@ -372,16 +504,13 @@ func (g *generator) walkMeshDir(path string) map[string]serviceTypes {
 		fs = map[string]serviceTypes{}
 	)
 
-	files, _ := ioutil.ReadDir(path)
-	for _, fi := range files {
-		if isStaticSkipFolder(fi.Name()) {
-			continue
-		}
-		if !fi.IsDir() {
-			continue
+	for _, servName := range g.cfg.Service {
+		if !existFile(servName) {
+			log.fault("service %s dir not found", servName)
 		}
 
-		fs[fi.Name()] = g.walkSubDir(fi.Name())
+		serv := g.walkSubDir(servName)
+		fs[servName] = serv
 	}
 
 	return fs
@@ -408,13 +537,10 @@ func (g *generator) walkSubDir(path string) serviceTypes {
 			sd.dm = "dm.yaml"
 
 		case "service.yaml":
-			sd.service = ""
+			sd.service = "service.yaml"
 
 		case "configmap.yaml":
 			sd.configMap = "configmap.yaml"
-
-		case "sync.sh":
-			sd.sync = "sync.sh"
 
 		case "istio.yaml":
 			sd.istio = "istio.yaml"
@@ -448,6 +574,10 @@ func isExistInList(f string, list []string) bool {
 	return false
 }
 
+func removeLine(s string) string {
+	return strings.Replace(s, "\n", "", -1)
+}
+
 func checkBinPathExist(cmd string) bool {
 	_, err := exec.LookPath(cmd)
 	if err != nil {
@@ -475,11 +605,38 @@ var (
 	log = new(logger)
 )
 
+const (
+	blue = iota
+	yellow
+	green
+	bpink
+)
+
 type logger struct{}
 
 func (l *logger) info(format string, args ...interface{}) {
 	v := fmt.Sprintf(format, args...)
 	fmt.Println(v)
+}
+
+func (l *logger) color(color int, format string, args ...interface{}) {
+	var (
+		v   = fmt.Sprintf(format, args...)
+		str string
+	)
+
+	switch color {
+	case blue:
+		str = fmt.Sprintf("%c[%d;%d;%dm %s %c[0m", 0x1B, 36, 40, 1, v, 0x1B)
+	case yellow:
+		str = fmt.Sprintf("%c[%d;%d;%dm %s %c[0m", 0x1B, 33, 40, 4, v, 0x1B)
+	case green:
+		str = fmt.Sprintf("%c[%d;%d;%dm %s %c[0m", 0x1B, 32, 40, 1, v, 0x1B)
+	case bpink:
+		str = fmt.Sprintf("%c[%d;%d;%dm %s %c[0m", 0x1B, 30, 41, 5, v, 0x1B)
+	}
+
+	fmt.Println(str)
 }
 
 func (l *logger) error(format string, args ...interface{}) {
@@ -502,12 +659,65 @@ func existFile(name string) bool {
 	return true
 }
 
+func isArrayRepeat(sl []string) (string, bool) {
+	m := map[string]bool{}
+
+	for _, sn := range sl {
+		if _, ok := m[sn]; ok {
+			return sn, true
+		}
+		m[sn] = true
+	}
+
+	return "", false
+}
+
 func flagParse() {
 	flag.StringVar(&envFile, "env", "envfile", "env file")
 	flag.Parse()
 
 	if envFile == "" {
 		log.fault("envfile is null")
+	}
+}
+
+func oneCommand(gen *generator, args ...string) {
+	op := args[0]
+
+	switch op {
+	case "start":
+		gen.run()
+
+	case "stop":
+		gen.stopAll()
+
+	case "gen":
+		gen.generator()
+
+	case "restart":
+		gen.stopAll()
+		gen.run()
+
+	case "ps", "status":
+		gen.statusAll()
+
+	case "pull":
+		log.info("auto pull new image")
+
+	default:
+		log.fault("invalid args")
+	}
+}
+
+func multiCommand(gen *generator, args ...string) {
+	op := args[0]
+
+	switch op {
+	case "start":
+		// 1. get service or service_group
+	case "logs":
+		// 1. pipe
+		// 2. listen signal
 	}
 }
 
@@ -537,43 +747,6 @@ func main() {
 	if len(args) > 1 {
 		multiCommand(gen, args...)
 		return
-	}
-}
-
-func oneCommand(gen *generator, args ...string) {
-	op := args[0]
-
-	switch op {
-	case "start":
-		gen.run()
-
-	case "stop":
-		gen.stopAll()
-
-	case "restart":
-		gen.stopAll()
-		gen.run()
-
-	case "ps", "status":
-		gen.statusAll()
-
-	case "pull":
-		log.info("auto pull new image")
-
-	default:
-		log.fault("invalid args")
-	}
-}
-
-func multiCommand(gen *generator, args ...string) {
-	op := args[0]
-
-	switch op {
-	case "start":
-		// 1. get service or service_group
-	case "logs":
-		// 1. pipe
-		// 2. listen signal
 	}
 }
 
